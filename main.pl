305f46bbe87ae3c6272a741e63d463667d51e706
phreid <phreid@ualberta.ca>
2021-04-11 16:27:02 -0600

    improve terminal input a bit

--- main.pl
+++ main.pl
@@ -1,7 +1,21 @@
 :- use_module(api).
 
-main(Result) :-
-    write("Ask a question: "),
-    flush_output(current_output),
-    readln(Ln),
-    fetch_answer(Ln, Result).
+main :-
+    repeat,
+        write("Ask a question (or type 'quit' to exit): "),
+        flush_output(current_output),
+        readln(Ln),
+        check_answer(Ln),
+        writeln("\n"),
+        fail.
+
+check_answer([quit]) :- writeln("Goodbye."), abort.
+
+check_answer(Ln) :-
+    (
+        fetch_answer(Ln, Answer)
+    ->
+        write("The answer is: "), writeln(Answer)
+    ;
+        writeln("I'm sorry, I couldn't find an answer to that question.")
+    ).
